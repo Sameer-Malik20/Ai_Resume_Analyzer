ed54eff6c67e48bff01e9ed71e44f284136366f3
Sameer-Malik20 <sameermalik63901@gmail.com>
2025-05-06 17:53:23 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -160,4 +160,4 @@ def download_file(filename):
         return jsonify({"error": f"Failed to download file: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
